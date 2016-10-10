bf3730d56ef116ea459eacd14caed44d3167f5d0
narrowdesign <nick@narrowdesign.com>
2016-10-10 14:20:13 -0400

    states

--- ui/assets/scripts/script.js
+++ ui/assets/scripts/script.js
@@ -5,7 +5,7 @@ var moduleTops = [];
 
 document.addEventListener('scroll',scrollHandler);
 window.addEventListener('resize',resizeHandler);
-
+var mollys = ['Inactive','Waking','Serious','Urgent','Serious','Active','Logo','Grid','Thinking','Designing','Cool!','Doing Stuff']
 resizeHandler();
 setTimeout(setTops,1);
 
@@ -28,20 +28,22 @@ function scrollHandler(e) {
 function resizeHandler(e) {
 	setTops();
 }
-readMessage('error')
-function readMessage(message) {
-	var i=0;
-	// setInterval(
-	// 	function(){
-	// 		document.querySelector('.logo').innerHTML = message.substr(i,1);
-	// 		i++;
-	// 		if (i == message.length) {
-	// 			i= 0;
-	// 		}
-	// 	}
-
-	// ,200)
+function nextMolly(num) {
+	document.querySelector('.icon').src = '/ui/assets/images/molly' + num + '.png';
+	document.querySelector('.type').innerHTML = mollys[num-1]
 }
+var i=1;
+nextMolly(i);
+setInterval(
+	function(){
+		i++;
+		nextMolly(i)
+		if (i == mollys.length) {
+			i = 1;
+		}
+	}
+
+,2000)
 
 function setTops() {
 	moduleTops = []
