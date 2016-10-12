c0b656ce5acb74084d4bc73d1e9bc3bb72908811
narrowdesign <nick@narrowdesign.com>
2016-10-12 00:43:11 -0400

    meet molly

--- ui/assets/scripts/script.js
+++ ui/assets/scripts/script.js
@@ -2,6 +2,7 @@ var scrollTop = 0;
 var modules = document.querySelectorAll('.Module')
 var menuItems = document.querySelectorAll('.Menu-item')
 var moduleTops = [];
+var sections = document.querySelectorAll('section');
 
 document.addEventListener('scroll',scrollHandler);
 window.addEventListener('resize',resizeHandler);
@@ -28,10 +29,102 @@ function scrollHandler(e) {
 function resizeHandler(e) {
 	setTops();
 }
+
+function activeFirst(argument) {
+	sections[0].classList.remove('current');
+	sections[1].classList.add('current');
+	setTimeout(function(){
+		sections[1].querySelectorAll('li')[0].classList.remove('current');
+		sections[1].querySelectorAll('li')[0].classList.add('inactive');
+		sections[1].querySelectorAll('li')[1].classList.add('current');
+	},1000);
+
+	setTimeout(function(){
+		sections[1].querySelectorAll('li')[1].classList.remove('current');
+		sections[1].querySelectorAll('li')[1].classList.add('inactive');
+		sections[1].querySelectorAll('li')[2].classList.add('current');
+	},3000);
+
+	setTimeout(function(){
+		sections[1].querySelectorAll('li')[2].classList.remove('current');
+		sections[1].querySelectorAll('li')[2].classList.add('inactive');
+		sections[1].querySelectorAll('li')[3].classList.add('current');
+	},5000);
+
+	setTimeout(function(){
+		sections[1].querySelectorAll('li')[3].classList.remove('current');
+		sections[1].querySelectorAll('li')[3].classList.add('inactive');
+		sections[1].querySelectorAll('li')[4].classList.add('current');
+		sections[1].querySelectorAll('li')[4].classList.add('current');
+		sections[1].querySelectorAll('li')[5].classList.add('current');
+	},6500);
+}
+
+sections[0].querySelector('.submit').addEventListener('click', function() {
+	activeFirst()
+})
+
+sections[1].querySelector('.submit').addEventListener('click', function(){
+	sections[1].classList.remove('current');
+	sections[2].classList.add('current');
+	sections[2].querySelectorAll('li')[0].classList.add('current');
+})
+
+sections[2].querySelector('.submit').addEventListener('click', function(){
+	sections[2].classList.remove('current');
+	sections[3].classList.add('current');
+	sections[3].querySelectorAll('li')[0].classList.add('current');
+})
+
+sections[3].querySelector('.submit').addEventListener('click', function(){
+	sections[3].classList.remove('current');
+	sections[4].classList.add('current');
+	sections[4].querySelectorAll('li')[0].classList.add('current');
+})
+sections[4].querySelector('.submit').addEventListener('click', function(){
+	sections[4].classList.remove('current');
+	sections[5].classList.add('current');
+	sections[5].querySelectorAll('li')[0].classList.add('current');
+})
+sections[4].querySelector('.submit').addEventListener('click', function(){
+	sections[4].classList.remove('current');
+	sections[5].classList.add('current');
+	sections[5].querySelectorAll('li')[0].classList.add('current');
+})
+sections[5].querySelector('.submit').addEventListener('click', function(){
+	sections[5].classList.remove('current');
+	sections[6].classList.add('current');
+	sections[6].querySelectorAll('li')[0].classList.add('current');
+})
+sections[6].querySelector('.submit').addEventListener('click', function(){
+	sections[6].classList.remove('current');
+	sections[7].classList.add('current');
+	sections[7].querySelectorAll('li')[0].classList.add('current');
+})
+sections[7].querySelector('.submit').addEventListener('click', function(){
+	sections[7].classList.remove('current');
+	sections[8].classList.add('current');
+	sections[8].querySelectorAll('li')[0].classList.add('current');
+})
+sections[8].querySelector('.submit').addEventListener('click', function(){
+	sections[8].classList.remove('current');
+	sections[9].classList.add('current');
+	sections[9].querySelectorAll('li')[0].classList.add('current');
+})
+sections[9].querySelector('.submit').addEventListener('click', function(){
+	sections[9].classList.remove('current');
+	sections[10].classList.add('current');
+	sections[10].querySelectorAll('li')[0].classList.add('current');
+})
+sections[10].querySelector('.submit').addEventListener('click', function(){
+	window.location.reload()
+})
+
 function nextMolly(num) {
 	document.querySelector('.icon').src = 'ui/assets/images/molly' + num + '.png';
 	document.querySelector('.type').innerHTML = mollys[num-1]
 }
+
 var i=1;
 nextMolly(i);
 setInterval(
